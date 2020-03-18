5f57ff6c7dd284f224fe44d83525988ee0afbce3
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-18 01:06:59 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,11 +1,13 @@
-class Artist 
+class Appointment 
   
-  attr_accessor :name
+  attr_accessor :date, :patient, :doctor
   
   @@all = []
   
-  def initialize(name)
-    @name = name 
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
     @@all << self
   end 
   
