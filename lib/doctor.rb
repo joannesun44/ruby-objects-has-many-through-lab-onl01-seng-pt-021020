c8374288a4d882856591278149e674ed5c8ff90c
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-18 01:48:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,8 +16,8 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end 
   
-  def new_appointment
-    
+  def new_appointment(date,patient, appointment)
+    Appointment.new(date, patient, self)
   end 
   
 end 
