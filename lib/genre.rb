7677752812c87ab8d7b57cff27ebc9ebef1eee61
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-18 01:28:39 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -8,9 +8,17 @@ class Genre
   @@all << self
   end 
   
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
+  
+  def songs 
+    Song.all.select{|song| song.genre == self}
+  end 
+  
+  
+  
+  
   
   
 end 
